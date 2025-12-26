c843bfabd17e68560c73438300f65aa69c4dfb77
unknown <charlie12negi@gmail.com>
2025-12-26 10:13:44 +0530

    arrays problem 3

---
+++ Arrays/search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,22 @@
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            mid=low+(high-low)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>=nums[low]:
+                if nums[low]<=target<=nums[mid]:
+                    high=mid-1
+                else:
+                    low=mid+1
+            else:
+                if nums[mid]<=target<=nums[high]:
+                    low=mid+1
+                else:
+                    high=mid-1
+        return -1
+    
+print(Solution.search(self="",nums = [4,5,6,7,0,1,2], target = 0))
